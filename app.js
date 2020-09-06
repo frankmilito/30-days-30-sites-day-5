0cb33edf009a1b51fc7cd90a9bb49fa74f0ed1fb
frankmilito <frankobidike20@gmail.com>
2020-09-06 00:30:45 -0500

    Day 5 challenge

---
+++ app.js
@@ -0,0 +1,28 @@
+var menu = document.getElementById("menu");
+var nav = document.getElementById("nav");
+var exit = document.getElementById("exit");
+
+menu.addEventListener("click", function (e) {
+  // nav.classList.toggle("menu-items");
+  nav.style.width = "100%";
+  e.preventDefault();
+});
+exit.addEventListener("click", function (e) {
+  // nav.classList.add("menu-items");
+  nav.style.width = "0";
+  e.preventDefault();
+});
+
+// When the user scrolls the page, execute myFunction
+// window.onscroll = function () {
+//   myFunction();
+// };
+
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height =
+//     document.documentElement.scrollHeight -
+//     document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
